a0dd2a1b13d42f81ff57c3a508e1621bc39e02d2
jeff-burns <1jeffreyburns@gmail.com>
2018-06-20 17:54:17 -0600

    added email and copy button, also fixed some styling

--- app.js
+++ app.js
@@ -10,3 +10,27 @@ function toggle_visibility(id) {
      var e = document.getElementById(id);
       return e.style.display = 'none';
  }
+
+ var copyEmailBtn = document.querySelector('.js-emailcopybtn');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Select the email link anchor text  
+  var emailLink = document.querySelector('.js-emaillink');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+
+  try {  
+    // Now that we've selected the anchor text, execute the copy command  
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);
+    var copied = document.getElementById('copied')  
+    copied.textContent = "Copied!"
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+
+  // Remove the selections - NOTE: Should use
+  // removeRange(range) when it is supported  
+  window.getSelection().removeAllRanges();  
+});
